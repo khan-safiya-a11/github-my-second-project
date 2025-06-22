7b1b92340ff94337a4889b6abfe53447640c7b4b
safiya khan <safiyakhan5850@gmail.com>
2025-06-22 17:42:24 +0530

    add this

---
+++ src/main/java/com/github/github_my_second_project/GithubMySecondProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.github_my_second_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMySecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMySecondProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/github_my_second_project/GithubMySecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.github_my_second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubMySecondProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
